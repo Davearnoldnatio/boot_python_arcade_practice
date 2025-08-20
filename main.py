e2ae6171263a6df62b234dd729829aa055c73f82
Davearnoldnatio <davearnoldnation@gmail.com>
2025-08-20 16:28:08 -0500

    fixed dt, added asteroid spawner, fixed asteroid class, fixed containers

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 
@@ -15,9 +17,16 @@ def main():
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()    
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     _ = player
+    asteroid_field = AsteroidField()
+    _ = asteroid_field
+    
 
 
     print("Starting Asteroids!")
@@ -26,9 +35,9 @@ def main():
     
     while True:
         for event in pygame.event.get():
-            dt = clock.tick(60) / 1000
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
         screen.fill(color="black")
         for item in drawable:
             item.draw(screen)
